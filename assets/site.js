a665469a8014ce5e0a68d12c358602161f73f50e
MD HELAL AHAMED <65183207+helal4flute@users.noreply.github.com>
2026-08-19 20:58:51 -0400

    Delete assets directory

--- assets/site.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  const toggle=document.querySelector('.mobile-toggle');
-  const links=document.querySelector('.nav-links');
-  if(toggle&&links){toggle.addEventListener('click',()=>links.classList.toggle('open'));}
-  document.querySelectorAll('.nav-links a').forEach(a=>a.addEventListener('click',()=>links&&links.classList.remove('open')));
-})();
